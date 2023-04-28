ce7e3531c5fd9e60b789d7b03aaaf384f1138676
Ritz6533 <riteshjungshah6533@gmail.com>
2023-04-28 17:43:10 +0100

    added navigation intent

--- app/src/main/java/com/example/assignment2/Shop.java
+++ app/src/main/java/com/example/assignment2/Shop.java
@@ -1,9 +1,13 @@
 package com.example.assignment2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -13,53 +17,72 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class Shop extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class Shop extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DbHelper DB;
 
     Button s;
     String TheChannelID = "channel id";
-    String TheChannelName = "channel name";
-    String Descriptionis = "hehehehehehehehehehe";
+    String TheChannelName = "ce";
+    String Descriptionis = "a";
     int notificationid = 0;
+    ImageView menuIcon;
+    LinearLayout contentView;
+
+    //Drawer Menu
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_shop);
+
+        //Hooks
+        menuIcon = findViewById(R.id.menu_icon);
+        contentView = findViewById(R.id.content);
+
+        //Menu Hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+
+        naviagtionDrawer();
 
         DB = new DbHelper(this);
 
 
-        s= findViewById(R.id.s);
+        s = findViewById(R.id.s);
         s.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),Login.class);
+                Intent intent = new Intent(getApplicationContext(), Login.class);
 
                 startActivity(intent);
-                Log.d("RUN ","Activity Started ");
+                Log.d("RUN ", "Activity Started ");
             }
         });
-        boolean login=true;
+        boolean login = true;
 
-        if(login=true){
-             notificationadd();}
-        else{
-            Log.d("MSG ","No LoggedIn ");
+        if (login = true) {
+            notificationadd();
+        } else {
+            Log.d("MSG ", "No LoggedIn ");
         }
 
     }
-    @Override
-    public void onBackPressed() {
 
-        moveTaskToBack(true);
-    }
-
-    private void notificationadd(){
+    private void notificationadd() {
         createNotificationChannel();
 
         Intent intent = new Intent(this, Shop.class);
@@ -90,7 +113,6 @@ public class Shop extends AppCompatActivity {
         notificationManager.notify(notificationid, builder.build());
 
 
-
     }
 
 
@@ -108,6 +130,54 @@ public class Shop extends AppCompatActivity {
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
+
+    }
+
+    private void naviagtionDrawer() {
+
+        //Naviagtion Drawer
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_shop);
+
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (drawerLayout.isDrawerVisible(GravityCompat.START))
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                else drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else
+            super.onBackPressed();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            switch (item.getItemId()) {
+                case R.id.nav_shop:
+                    onBackPressed();
+                    break;
+                case R.id.nav_logout:
+                    Intent intent = new Intent(Shop.this, Login.class);
+                    startActivity(intent);
+                case R.id.nav_profile:
+                    Toast.makeText(Shop.this, "Clicked the profile", Toast.LENGTH_SHORT).show();
+
+                //creating the onclick action for the nav menu
+            }
+            return true;
 
     }
 }
